675b4e4013c778e814ba6552b536d7dc800bc558
Sergio Ramos <sergioislasr22@gmail.com>
2024-01-04 03:22:52 -0600

    upload project to github

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Jorge Sergio Islas Ramos on 14/10/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
